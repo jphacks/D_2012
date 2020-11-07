49112f5419d0de5ee01e152047354630341d8e49
take0908 <39321834+take0908@users.noreply.github.com>
2020-11-07 12:48:52 +0900

    frontend

---
+++ macos/state/Assets.xcassets/statusbar/statusbar/AppDelegate.dataset/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  statusbar
+//
+//  Created by 武久宗平 on 2020/10/31.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macos/state/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  state
+//
+//  Created by 武久宗平 on 2020/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
